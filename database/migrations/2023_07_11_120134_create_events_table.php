4fc702b863ed37f4a4369e4065eaddee459bde9b
febriyantiazahra <febriyantiazhraa@gmail.com>
2023-07-15 20:38:14 +0700

    update migration event table

--- database/migrations/2023_07_11_120134_create_events_table.php
+++ database/migrations/2023_07_11_120134_create_events_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('event');
+            $table->text('description');
+            $table->integer('jumlah');
             $table->boolean('is_complete')->default(0);
             $table->timestamps();
         });
